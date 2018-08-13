2c596611a7dbb405c0246b18882e1cac6a98a8f8
Poison-Fang <ayushwalia999@gmail.com>
2018-08-13 09:47:16 +0000

    Lockscreen Date fonts
    Change-Id: I3c7ebe54ca9e70811cab335a58d0c1fce51896d6

--- src/com/android/settings/rr/LockScreenUI.java
+++ src/com/android/settings/rr/LockScreenUI.java
@@ -34,7 +34,9 @@ public class LockScreenUI extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "LockScreenSecurity";
     private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+    private static final String LOCK_DATE_FONTS = "lock_date_fonts";
     ListPreference mLockClockFonts;
+    ListPreference mLockDateFonts;
 
     @Override
     public int getMetricsCategory() {
@@ -53,6 +55,13 @@ public class LockScreenUI extends SettingsPreferenceFragment implements
                 getContentResolver(), Settings.System.LOCK_CLOCK_FONTS, 0)));
         mLockClockFonts.setSummary(mLockClockFonts.getEntry());
         mLockClockFonts.setOnPreferenceChangeListener(this);
+
+        // Lockscren Date Fonts
+        mLockDateFonts = (ListPreference) findPreference(LOCK_DATE_FONTS);
+        mLockDateFonts.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.LOCK_DATE_FONTS, 26)));
+        mLockDateFonts.setSummary(mLockDateFonts.getEntry());
+        mLockDateFonts.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) 		{
@@ -63,6 +72,12 @@ public class LockScreenUI extends SettingsPreferenceFragment implements
             mLockClockFonts.setValue(String.valueOf(newValue));
             mLockClockFonts.setSummary(mLockClockFonts.getEntry());
             return true;
+        } else if (preference == mLockDateFonts) {
+                Settings.System.putInt(getContentResolver(), Settings.System.LOCK_DATE_FONTS,
+                        Integer.valueOf((String) newValue));
+                mLockDateFonts.setValue(String.valueOf(newValue));
+                mLockDateFonts.setSummary(mLockDateFonts.getEntry());
+                return true;
         }
         return false;
     }
